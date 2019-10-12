65573b59ed784a4cca55094036f3dac46f6d0e75
zeebot <jjclark@attain.com>
2019-10-11 19:45:19 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="playerOneStart">
+        <div id="playerOneBallOneStart" className="playerOneBall"></div>
+        <div id="playerOneBallTwoStart" className="playerOneBall"></div>
+        <div id="playerOneBallThreeStart"className="playerOneBall"></div>
+        <div id="playerOneBallFourStart" className="playerOneBall"></div>
+      </div>
+      <div id="playerTwoStart">
+        <div id="playerTwoBallOneStart" className="playerTwoBall"></div>
+        <div id="playerTwoBallTwoStart" className="playerTwoBall"></div>
+        <div id="playerTwoBallThreeStart"className="playerTwoBall"></div>
+        <div id="playerTwoBallFourStart" className="playerTwoBall"></div>
+      </div>
+      <div id="playerThreeStart">
+        <div id="playerThreeBallOneStart" className="playerThreeBall"></div>
+        <div id="playerThreeBallTwoStart" className="playerThreeBall"></div>
+        <div id="playerThreeBallThreeStart"className="playerThreeBall"></div>
+        <div id="playerThreeBallFourStart" className="playerThreeBall"></div>
+      </div>
+      <div id="playerFourStart">
+        <div id="playerFourBallOneStart" className="playerFourBall"></div>
+        <div id="playerFourBallTwoStart" className="playerFourBall"></div>
+        <div id="playerFourBallThreeStart"className="playerFourBall"></div>
+        <div id="playerFourBallFourStart" className="playerFourBall"></div>
+      </div>
+      <div id="middlePostion"></div>
     </div>
   );
 }

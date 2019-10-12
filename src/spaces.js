4912bee1e6a883e0242449368635ec65c7da4921
zeebot <jjclark@attain.com>
2019-10-11 22:23:41 -0700

    added half of the spaces

--- src/spaces.js
+++ src/spaces.js
@@ -73,6 +73,161 @@ export const spaces = [
         id: 15,
         top: "139px",
         left: "511px"
+    },
+    {
+        id: 16,
+        top: "201px",
+        left: "511px"
+    },
+    {
+        id: 17,
+        top: "263px",
+        left: "511px"
+    },
+    {
+        id: 18,
+        top: "263px",
+        left: "573px"
+    },
+    {
+        id: 19,
+        top: "263px",
+        left: "635px"
+    },
+    {
+        id: 20,
+        top: "263px",
+        left: "697px"
+    },
+    {
+        id: 21,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 22,
+        top: "325px",
+        left: "759px"
+    },
+    {
+        id: 23,
+        top: "387px",
+        left: "759px"
+    },
+    {
+        id: 24,
+        top: "449px",
+        left: "759px"
+    },
+    {
+        id: 25,
+        top: "511px",
+        left: "759px"
+    },
+    {
+        id: 26,
+        top: "511px",
+        left: "697px"
+    },
+    {
+        id: 27,
+        top: "511px",
+        left: "635px"
+    },
+    {
+        id: 28,
+        top: "511px",
+        left: "573px"
+    },
+    {
+        id: 29,
+        top: "511px",
+        left: "511px"
+    },
+    {
+        id: 30,
+        top: "573px",
+        left: "511px"
+    },
+    {
+        id: 31,
+        top: "635px",
+        left: "511px"
+    },
+    {
+        id: 32,
+        top: "697px",
+        left: "511px"
+    },
+    {
+        id: 33,
+        top: "759px",
+        left: "511px"
+    },
+    {
+        id: 34,
+        top: "759px",
+        left: "449px"
+    },
+    {
+        id: 35,
+        top: "759px",
+        left: "387px"
+    },
+    {
+        id: 36,
+        top: "759px",
+        left: "325px"
+    },
+    {
+        id: 37,
+        top: "759px",
+        left: "263px"
+    },
+    {
+        id: 38,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 39,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 40,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 41,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 42,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 43,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 44,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 45,
+        top: "263px",
+        left: "759px"
+    },
+    {
+        id: 46,
+        top: "263px",
+        left: "759px"
     }
 ]
 

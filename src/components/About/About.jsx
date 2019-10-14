9b94a9b780ed2e8331bb0b67e041d68794757b88
zeebot <jjclark@attain.com>
2019-10-14 14:58:36 -0700

    added basic information about the game

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
     return(
-        <p>About page</p>
+        <div>
+            <h1>What is Aggravation?</h1>
+            <p>Aggravation is a board game for up to four players and later versions for up to six players, whose object is to be the first player to have all four playing pieces (usually represented by marbles) reach the player's home section of the board. The game's name comes from the action of capturing an opponent's piece by landing on its space, which is known as "aggravating".</p>
+
+            <p>Aggravation is one of the many variations of the game Pachisi. It was first produced in 1960 by CO-5 Company. Later versions were made by Minneapolis-based Lakeside Industries, a division of Leisure Dynamic. Today, it is manufactured by Winning Moves, under license from Hasbro.</p>
+            
+            <p>Its distinctive features are that the track accommodates from four to six players, unlike other Pachisi-like games which only allow four; that it is normally drilled to accept colored glass marbles as playing pieces; and that it incorporates "shortcuts". There are no "safe" holes where a player's marbles cannot be captured (or "aggravated", in the game's parlance) other than the player's own base and home sections.</p>
+            
+            <p>Older versions of the game usually feature an asterisk-shaped board, which is perfectly symmetrical and identical in shape and size from all angles. In addition, older versions allowed up to four players instead of six. However, modern versions of the game produced by Parker Brothers are made in an irregular pattern with a shape that varies for each player, though all players must travel an equal number of spaces in order to reach their respective home sections</p>
+        </div>
+        
     )
 }
 

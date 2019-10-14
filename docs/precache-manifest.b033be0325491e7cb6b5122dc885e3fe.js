7424c486758a1ef7751eadb03d1243074a2315ba
zeebot <jjclark@attain.com>
2019-10-14 09:29:20 -0700

    added build script to copy build to docs folder to be able to share on github pages

---
+++ docs/precache-manifest.b033be0325491e7cb6b5122dc885e3fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b9070d6eb8f3b6c7612d5dfa59f39f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bf328e539434c642ea7",
+    "url": "/static/css/2.466c2356.chunk.css"
+  },
+  {
+    "revision": "a0ce40bf7034cd1855a6",
+    "url": "/static/css/main.9a9aff4f.chunk.css"
+  },
+  {
+    "revision": "3bf328e539434c642ea7",
+    "url": "/static/js/2.eb226aee.chunk.js"
+  },
+  {
+    "revision": "a0ce40bf7034cd1855a6",
+    "url": "/static/js/main.92b3652d.chunk.js"
+  },
+  {
+    "revision": "8a323d056fb0ceb4d901",
+    "url": "/static/js/runtime-main.910c7e12.js"
+  },
+  {
+    "revision": "3d75d7a838059fd55976cc115020c118",
+    "url": "/static/media/Kurumi-Table-Top-top.3d75d7a8.jpg"
+  }
+]);

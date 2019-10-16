e092c0b2e9d06c59dc01fb22c044c4ab696ff521
zeebot <jjclark@attain.com>
2019-10-16 16:52:27 -0700

    we are now checking who the current player is

--- src/components/Game/GameBoard.jsx
+++ src/components/Game/GameBoard.jsx
@@ -44,7 +44,8 @@ class GameBoard extends React.Component {
       previousBall: null,
       currentBall: null,
       count: 0,
-      leftMargin: null
+      leftMargin: null,
+      currentPlayer: "green"
     }
 
     this.gameBoard = React.createRef();
@@ -62,48 +63,50 @@ class GameBoard extends React.Component {
 
   playerStart = (event) => {
     let currentBall = event.target;
-    // If the player rolls a 1 or a 6 they can choose a starter
-    // ball to put into play as long as there isn't one in their 
-    // starting space currently.
-    if(this.state.playerRoll === 6 || this.state.playerRoll === 1){
-      if(currentBall.classList.contains('playerOneBallStart')){
-        this.setState({playerOneFirstMove: true});
+    console.log(currentBall.getAttribute('ballcolor'));
+    if(currentBall.getAttribute('ballcolor') !== this.state.currentPlayer){
+      alert("It is not your turn!\nIt is " + this.state.currentPlayer + "'s turn.")
+    }else{
+      // If the player rolls a 1 or a 6 they can choose a starter
+      // ball to put into play as long as there isn't one in their 
+      // starting space currently.
+      if(this.state.playerRoll === 6 || this.state.playerRoll === 1){
+        if(currentBall.classList.contains('playerOneBallStart')){
+          this.setState({playerOneFirstMove: true});
+        }
+        if(currentBall.classList.contains('playerTwoBallStart')){
+          this.setState({playerTwoFirstMove: true});
+        }
+        if(currentBall.classList.contains('playerThreeBallStart')){
+          this.setState({playerThreeFirstMove: true});
+        }
+        if(currentBall.classList.contains('playerFourBallStart')){
+          this.setState({playerFourFirstMove: true});
+        }
+        if(this.state.count === 0){
+          currentBall.classList.add('highlightBall');
+          this.setState({previousBall: currentBall});
+          this.setState({count: 1});
+        }else{
+          this.setState({count: 1});
+          //alert("You cannot move here");
+          currentBall.classList.add('highlightBall');
+          this.state.previousBall.classList.remove('highlightBall');
+          this.setState({previousBall: currentBall});
+        }
       }
-      if(currentBall.classList.contains('playerTwoBallStart')){
-        this.setState({playerTwoFirstMove: true});
+      else if(this.state.count === 1){
+        alert("You cannot move here");
       }
-      if(currentBall.classList.contains('playerThreeBallStart')){
-        this.setState({playerThreeFirstMove: true});
+      else{
+        alert("You must roll a 6 or a 1 to start playing!");
       }
-      if(currentBall.classList.contains('playerFourBallStart')){
-        this.setState({playerFourFirstMove: true});
-      }
-      if(this.state.count === 0){
-        currentBall.classList.add('highlightBall');
-        this.setState({previousBall: currentBall});
-        this.setState({count: 1});
-      }else{
-        this.setState({count: 1});
-        //alert("You cannot move here");
-        currentBall.classList.add('highlightBall');
-        this.state.previousBall.classList.remove('highlightBall');
-        this.setState({previousBall: currentBall});
-      }
-    }
-    else if(this.state.count === 1){
-      alert("You cannot move here");
-    }
-    else{
-      alert("You must roll a 6 or a 1 to start playing!");
     }
   }
 
   moveAroundBoard = (event) => {
     let currentBall = event.target;
     console.log("current ball", currentBall);
-
-
-
 
     if(this.state.previousBall == null){
       alert('Roll a dice to begin playing');
@@ -237,9 +240,30 @@ class GameBoard extends React.Component {
       //do nothing
     }
   }
+
+  getCurrentPlayer = (event) => {
+    //If the user clicks on the dot we want to see 
+    //what the dice color is... not the dot.. so we grab
+    //the parent element to determine which dice is rolled
+    //and set the current player state to the dice color
+    if (event.target.classList.contains('dot')) {
+      //this.setState({ currentPlayer: event.target.parentElement.style.background });
+      this.checkPlayerTurn(event.target.parentElement.style.background)
+    }
+    else {
+      //this.setState({ currentPlayer: event.target.style.background });
+      this.checkPlayerTurn(event.target.style.background);
+    }
+  };
+
+  checkPlayerTurn = (playerElementBeingClicked) => {
+    if(playerElementBeingClicked !== this.state.currentPlayer){
+      alert("It is not your turn to play!\nIt is " + this.state.currentPlayer + "'s turn.");
+    }
+  }
+
     
   render(){
-
 
     return (
         <div ref={this.gameBoard} className="GameBoard" style={{
@@ -248,56 +272,68 @@ class GameBoard extends React.Component {
         }}>
           {/* starting spaces */}
           <div id="playerOneStart">
-            <div id="greenDice">
+            <div 
+              id="greenDice"
+              onClick={this.getCurrentPlayer}
+            >
               <Dice 
                 id="greenDice"
                 color="green"
                 rollDoneCallback={this.rollDice}
               />
             </div>
-            <div id="playerOneBallOne" className="playerOneBallStart" onClick={this.playerStart} ></div>
-            <div id="playerOneBallTwo" className="playerOneBallStart" onClick={this.playerStart} ></div>
-            <div id="playerOneBallThree"className="playerOneBallStart" onClick={this.playerStart} ></div>
-            <div id="playerOneBallFour" className="playerOneBallStart" onClick={this.playerStart} ></div>
+            <div id="playerOneBallOne" className="playerOneBallStart" ballcolor="green" onClick={this.playerStart} ></div>
+            <div id="playerOneBallTwo" className="playerOneBallStart" ballcolor="green" onClick={this.playerStart} ></div>
+            <div id="playerOneBallThree"className="playerOneBallStart" ballcolor="green" onClick={this.playerStart} ></div>
+            <div id="playerOneBallFour" className="playerOneBallStart" ballcolor="green" onClick={this.playerStart} ></div>
           </div>
           <div id="playerTwoStart">
-            <div id="blueDice">
+            <div 
+              id="blueDice"
+              onClick={this.getCurrentPlayer}
+            >
               <Dice 
                 id="blueDice"
                 color="blue"
                 rollDoneCallback={this.rollDice}
               />
             </div>
-            <div id="playerTwoBallOne" className="playerTwoBallStart" onClick={this.playerStart}></div>
-            <div id="playerTwoBallTwo" className="playerTwoBallStart" onClick={this.playerStart}></div>
-            <div id="playerTwoBallThree"className="playerTwoBallStart" onClick={this.playerStart}></div>
-            <div id="playerTwoBallFour" className="playerTwoBallStart" onClick={this.playerStart}></div>
+            <div id="playerTwoBallOne" className="playerTwoBallStart" ballcolor="blue" onClick={this.playerStart}></div>
+            <div id="playerTwoBallTwo" className="playerTwoBallStart" ballcolor="blue" onClick={this.playerStart}></div>
+            <div id="playerTwoBallThree"className="playerTwoBallStart" ballcolor="blue" onClick={this.playerStart}></div>
+            <div id="playerTwoBallFour" className="playerTwoBallStart" ballcolor="blue" onClick={this.playerStart}></div>
           </div>
           <div id="playerThreeStart">
-            <div id="redDice">
+            <div 
+              id="redDice"
+              onClick={this.getCurrentPlayer}
+            >
               <Dice 
                 id="redDice"
                 color="red"
                 rollDoneCallback={this.rollDice}
               />
             </div>
-            <div id="playerThreeBallOne" className="playerThreeBallStart" onClick={this.playerStart}></div>
-            <div id="playerThreeBallTwo" className="playerThreeBallStart" onClick={this.playerStart}></div>
-            <div id="playerThreeBallThree"className="playerThreeBallStart" onClick={this.playerStart}></div>
-            <div id="playerThreeBallFour" className="playerThreeBallStart" onClick={this.playerStart}></div>
+            <div id="playerThreeBallOne" className="playerThreeBallStart" ballcolor="red" onClick={this.playerStart}></div>
+            <div id="playerThreeBallTwo" className="playerThreeBallStart" ballcolor="red" onClick={this.playerStart}></div>
+            <div id="playerThreeBallThree"className="playerThreeBallStart" ballcolor="red" onClick={this.playerStart}></div>
+            <div id="playerThreeBallFour" className="playerThreeBallStart" ballcolor="red" onClick={this.playerStart}></div>
           </div>
           <div id="playerFourStart">
-            <div id="yellowDice">
+            <div 
+              id="yellowDice"
+              onClick={this.getCurrentPlayer}
+            >
               <Dice 
                 id="yellowDice"
                 color="yellow"
                 rollDoneCallback={this.rollDice}
               />
             </div>
-            <div id="playerFourBallOne" className="playerFourBallStart" onClick={this.playerStart}></div>
-            <div id="playerFourBallTwo" className="playerFourBallStart" onClick={this.playerStart}></div>
-            <div id="playerFourBallThree"className="playerFourBallStart" onClick={this.playerStart}></div>
-            <div id="playerFourBallFour" className="playerFourBallStart" onClick={this.playerStart}></div>
+            <div id="playerFourBallOne" className="playerFourBallStart" ballcolor="yellow" onClick={this.playerStart}></div>
+            <div id="playerFourBallTwo" className="playerFourBallStart" ballcolor="yellow" onClick={this.playerStart}></div>
+            <div id="playerFourBallThree"className="playerFourBallStart" ballcolor="yellow" onClick={this.playerStart}></div>
+            <div id="playerFourBallFour" className="playerFourBallStart" ballcolor="yellow" onClick={this.playerStart}></div>
           </div>
           {/* starting spaces end */}
           <div id="middlePostion" className={'spaces'} onClick={this.moveAroundBoard}></div>
